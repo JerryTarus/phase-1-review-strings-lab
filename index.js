469827c34fdb59aa08a3be8bb58603ebe364d772
Jerry Kipchirchir <jerrytarus@gmail.com>
2023-09-23 17:58:00 +0300

    Completed Strings Learning

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your code in this file!
+const currentUser = 'Jerry Tarus';
+
+const welcomeMessage = `Welcome to Flatbook +  ${currentUser}!`;
+
+
+const excitedWelcomeMessage = 'Welcome to Flatbook';
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+
+
+
+
+
+
+// const firstInitial = currentUser[0];
+// const restOfName = currentUser.slice(1);
+
+
